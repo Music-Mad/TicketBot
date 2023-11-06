003d16d106f50423b10dd4842117529a257ceb08
Music-Mad <chrisklepper1776@gmail.com>
2023-11-05 23:18:56 -0500

    Removed lib build files IDK WHY THIS PROJECT STILL RUNS HELP

--- build/library/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/TicketBot/dep/DPP/library
-# Build directory: /root/TicketBot/build/library
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unittests "library/unittest")
-set_tests_properties(unittests PROPERTIES  WORKING_DIRECTORY "/library" _BACKTRACE_TRIPLES "/root/TicketBot/dep/DPP/library/CMakeLists.txt;367;add_test;/root/TicketBot/dep/DPP/library/CMakeLists.txt;0;")
