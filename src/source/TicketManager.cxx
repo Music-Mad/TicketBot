7e3486cba2ab2208c2a93dfcf4178f40be6b1ed6
Music-Mad <chrisklepper1776@gmail.com>
2023-12-06 09:47:34 -0500

    Open Ticket now adds client to channel

--- src/source/TicketManager.cxx
+++ src/source/TicketManager.cxx
@@ -298,13 +298,54 @@ bool TicketManager::handleBtnPress(dpp::cluster& bot, const dpp::button_click_t&
 };
 
 bool TicketManager::publishTicket(const dpp::channel& channel, dpp::cluster& bot) {
+    //if ticket is already published return
+    if (channel.parent_id == dpp::snowflake(PUBLIC_CATEGORY_ID)) {
+        return false;
+    }
+
+    dpp::channel c = channel;
     try { 
-        dpp::channel c = channel;
         c.set_parent_id(PUBLIC_CATEGORY_ID);
         bot.channel_edit(c,[](auto const& callback) {});
-        return true;
     } catch( ... ) {
         std::cout << "ERROR: Failed to publish ticket\n";
         return false;
     }
+
+    //retrieve usr
+    bot.message_get(c.last_message_id, c.id, [&bot, c](const dpp::confirmation_callback_t& callback) {
+        if (callback.is_error()) {
+            std::cout << callback.get_error().message << std::endl;
+            return false;
+        }
+    
+        std::string usrId = "";
+        try {
+            const dpp::message& m = std::get<dpp::message>(callback.value);
+            for (int i = 0; i < m.content.length(); ++i ) {
+                char c = m.content[i];
+                if (c == '@') {
+                    usrId = m.content.substr(i + 1, 18);
+                }
+            }
+        } catch (...) {
+            std::cout << "ERROR: Failed tor retrieve usrId from ticket content\n";
+            return false;
+        }
+
+        try {
+            bot.channel_edit_permissions(c, dpp::snowflake(usrId), uint64_t(dpp::p_view_channel), uint64_t(0), true);
+            dpp::message notif(c.id, "<@" + usrId + "> Your commission has been approved and is now available to all Verified Creators. Please use this channel to coordinate with verifs and elaborate on your request.");
+            notif.set_allowed_mentions(true, true, true, true, std::vector<dpp::snowflake>(), std::vector<dpp::snowflake>());
+            bot.message_create(notif);
+        } catch (...) {
+            std::cout << "ERROR: Failed to add usr to channel";
+            return false;
+        }
+
+        return true;
+    });
+
+
+    return true;
 };
